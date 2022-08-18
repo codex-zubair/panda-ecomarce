8c4d55cf1492a3376b7c149d0a231292efc846c8
Rocko <codingwithrock@gmail.com>
2022-08-18 21:16:14 +0600

    3 added rerquirement

---
+++ panda.js
@@ -0,0 +1,26 @@
+
+// H2 Heading Lime Blue now
+const h2 = document.getElementsByTagName('h2')
+for (element of h2) {
+    element.style.color = 'lightblue'
+}
+
+
+// Back back section will Tomato by using id
+const backpackSection = document.getElementById('backpack')
+backpackSection.style.backgroundColor = 'tomato'
+
+
+
+
+// card Section find by id and make border radius 30 px
+const cardClass = document.getElementsByClassName('card')
+
+// looping for getting class.
+for (element of cardClass) {
+    element.style.borderRadius = '30px'
+}
+
+
+
+// 
